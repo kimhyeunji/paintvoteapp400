976aa26bcd2b458df1d2e5c67bcabeb7ae227d18
dasfef <mealhouse3377@gmail.com>
2023-05-25 13:14:38 +0900

    Initial commit 명화투표앱 p.401

---
+++ app/src/main/java/com/example/paintvoteapp400/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.paintvoteapp400;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("명화 선호도 투표");
+
+        final int voteCount[]= new int[9];
+        ImageView image[] = new ImageView[9];
+        Integer imageId[] = {R.id.iv1, R.id.iv2, R.id.iv3, R.id.iv4, R.id.iv5, R.id.iv6, R.id.iv7, R.id.iv8, R.id.iv9};
+        final String imgName[] = {"독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀", "이레느깡 단 베르양", "잠자는 소녀", "테라스의 두 자매", "피아노 레슨", "피아노 앞의 소녀들", "해변에서"};
+        for(int i = 0; i < 9; i++){
+            voteCount[i]=0;
+
+
+
+            for(int j = 0; j < imageId.length; j++){
+                final int index; //주의 꼭 필요함
+                index = j;
+                image[index] = findViewById(imageId[index]);
+                image[index].setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        voteCount[index]++;
+                        Toast.makeText(getApplicationContext(), imgName[index] + ": 총 " + voteCount[index] + "표", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+        Button btnFinish = findViewById(R.id.btnResult);
+        btnFinish.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),resultActivity2.class);
+                intent.putExtra("VoteCount",voteCount);
+                intent.putExtra("ImageName",imgName);
+
+                startActivity(intent);
+            }
+        });
+
+
+    }
+}
