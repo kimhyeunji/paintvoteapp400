3def766dc577ae42f3a530f2e2e0a320acbbecfb
dasfef <mealhouse3377@gmail.com>
2023-05-25 13:19:23 +0900

    깃 테스트

--- app/src/main/java/com/example/paintvoteapp400/MainActivity.java
+++ app/src/main/java/com/example/paintvoteapp400/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
+// 깃 테스트
 public class MainActivity extends AppCompatActivity {
 
     @Override
